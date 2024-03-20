1f08d53afb0c0c63427fe63edf0cbf854d6dd25b
Maggie-Njeri <mkiri00@gmail.com>
2024-03-20 14:39:22 -0700

    MYSQL

--- 0-setup_web_static.py
+++ 0-setup_web_static.py
@@ -2,7 +2,7 @@
 # Sets up a web server for deployment of web_static.
 
 apt-get update
-apt-get install -y nginx
+apt-get install - y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
@@ -19,18 +19,18 @@ printf %s "server {
     root   /var/www/html;
     index  index.html index.htm;
 
-    location /hbnb_static {
-        alias /data/web_static/current;
+    location / hbnb_static {
+        alias / data/web_static/current;
         index index.html index.htm;
     }
 
-    location /redirect_me {
+    location / redirect_me {
         return 301 http://cuberule.com/;
     }
 
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
+    error_page 404 / 404.html;
+    location / 404 {
+      root /var/www/html ;
       internal;
     }
 }" > /etc/nginx/sites-available/default
